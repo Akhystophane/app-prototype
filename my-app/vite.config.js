67d1aab5506646920c0e65ffd1f1d224893749b7
Akhystophane <emnl.busi@outlook.fr>
2025-04-13 14:32:16 -0400

    add build folder

--- my-app/vite.config.js
+++ my-app/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/app-prototype/",
   build: {
     // Put the build in a folder called "root"
     // relative to where this config file is located
