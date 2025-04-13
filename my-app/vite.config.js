7d158ba1a4340e9ece1ba3960dfef02f117e8794
Akhystophane <emnl.busi@outlook.fr>
2025-04-13 14:40:45 -0400

    fixed root folder location

--- my-app/vite.config.js
+++ my-app/vite.config.js
@@ -1,13 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: "/app-prototype/",
   build: {
-    // Put the build in a folder called "root"
-    // relative to where this config file is located
-    outDir: 'root',
+    outDir: '../root',
   }
 })
